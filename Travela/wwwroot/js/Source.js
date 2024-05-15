787166723146c5beda06cee3aa87451829a2ae97
acer <acer@PRO500AM4-M5>
2024-05-15 10:46:35 +0530

    Source

--- Travela/wwwroot/js/Source.js
+++ Travela/wwwroot/js/Source.js
@@ -1,1 +1,176 @@
-﻿
+﻿$(document).ready(function () {
+    console.log("Document ready");
+    BindGrid();
+
+    $('#addCityBtn').click(function () {
+        $('#addCityModal').modal('show');
+    });
+
+    $('#btnMdlSave').click(function () {
+
+        var cityName = $('#CityName').val();
+
+        if (!cityName) {
+            $('#cityError').text('Please enter City Name.');
+            return;
+        }
+
+        var formdata = new FormData($('#form')[0]);
+
+        $.ajax({
+            type: "POST",
+            url: "/City/AddOrUpdateCity",
+            data: formdata,
+            processData: false,
+            contentType: false,
+            dataType: 'json',
+            success: function (data) {
+                if (data != null && data != undefined) {
+                    alert(data.message);
+                    $('#addCityModal').modal('hide');
+                    BindGrid();
+                    $('#form')[0].reset();
+                }
+                else {
+                    alert("Record not saved, Try again", "", "error");
+                }
+            },
+            error: function (ex) {
+                alert("Something went wrong, Try again!", "", "error");
+            }
+        });
+    });
+});
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function EditModel(cityId) {
+    console.log("Hii");
+
+    $.ajax({
+        type: "GET",
+        url: "/City/EditCity",
+        data: { cityId: cityId }, // Pass cityId as a parameter
+        //    // Populate modal form fields with retrieved data
+        //    $('#CityName').val(data.cityName);
+        //    $('#IsActive').prop('checked', data.isActive);
+        //    // Show the modal
+        //    $('#addCityModal').modal('show');
+        //},
+        //error: function (ex) {
+        //    alert("Something went wrong, Try again!", "", "error");
+        //}
+        success: function (data) {
+            debugger;
+            if (data.isError) {
+                alert(data.strMessage);
+            } else {
+                var dataList = data.result;
+
+                Object.keys(dataList).forEach(function (key) {
+
+                    if ($('#' + capitalizeFirstLetter(key)) != null && $('#' + key) != undefined) {
+                        if (key.includes("is")) {
+                            $('#' + capitalizeFirstLetter(key)).prop('checked', dataList[key]);
+                        }
+                        else {
+                            $('#' + capitalizeFirstLetter(key)).val(dataList[key]);
+                        }
+                    }
+                });
+            }
+            $('#addCityModal').modal('show');
+        },
+        error: function (ex) {
+            ShowMessage("Something went wrong. Please try again.", "", "error");
+        }
+    });
+}
+
+function DeleteData(cityId, cityName) {
+    var row = $(this).closest('tr');
+
+    if (confirm('Are you sure you want to delete the city "' + cityName + '"?')) {
+        $.ajax({
+            type: "POST",
+            url: "/City/DeleteCity",
+            data: { cityId: cityId },
+            success: function (result) {
+                BindGrid();
+                alert('City deleted successfully.');
+            },
+            error: function () {
+                alert("An error occurred while deleting the city.");
+            }
+        });
+    }
+}
+
+function BindGrid() {
+    if ($.fn.DataTable.isDataTable("#tbldata")) {
+        $('#tbldata').DataTable().clear().destroy();
+    }
+    var yesBadge = '<td><span class="badge badge-info mt-1">Yes</span></td>';
+    var noBadge = '<td><span class="badge badge-secondary mt-1">No</span></td>';
+
+    $("#tbldata").DataTable({
+        "processing": true, // for show progress bar
+        "serverSide": false, // for process server side
+        "filter": true, // this is for disable filter (search box)
+        "orderMulti": false, // for disable multiple column at once
+        "initComplete": function () {
+            var api = this.api();
+            var searchInput = $('.dataTables_filter input');
+
+            searchInput.on('keyup change', function () {
+                if (searchInput.val() === '') {
+                    api.search('').draw();
+                }
+            });
+        },
+        "ajax": {
+            "url": "/City/GetCityData",
+            "contentType": "application/x-www-form-urlencoded",
+            "type": "POST",
+            "datatype": "json",
+            "dataSrc": function (json) {
+                console.log(json.data);
+                var jsonObj = json.data;
+                return jsonObj;
+            }
+        },
+        "columnDefs": [{
+            "targets": [0],
+            "searchable": false
+        }],
+        "columns": [
+            {
+                name: "City Id",
+                render: function (data, type, row, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                }, autoWidth: true
+            },
+            { data: "cityName", name: "City Name", autoWidth: true },
+            {
+                data: null,
+                render: function (data, type, row) {
+                    return row.isActive ? yesBadge : noBadge;
+
+
+
+                }, autoWidth: true
+            },
+            {
+                data: null,
+                render: function (data, type, row) {
+                    var editButton = "<button class=\"btn mb-0 btn-outline-success btnedit\" title=\"Edit\" onclick=\"EditModel('" + row.cityId + "');\">Edit<i class=\"fas fa-pencil-alt\"></i></button>&nbsp;";
+                    var deleteButton = "<button class=\"btn mb-0 btn-outline-danger btndelete\" title=\"Delete\" onclick=\"DeleteData('" + row.cityId + "', '" + row.cityName + "');\">Delete<i class=\"fas fa-trash-alt\"></i></button>";
+                    return editButton + deleteButton;
+                }, autoWidth: true
+            }
+        ]
+    });
+}
+
