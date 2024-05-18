a0ba40a6b69069fbf30db9dd328e67ca4d39c88c
acer <acer@PRO500AM4-M5>
2024-05-18 17:31:32 +0530

    Hotel Model

---
+++ Travela.Model/Service/HotelModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travela.Model.Service
+{
+    public class HotelModel
+    {
+        public int hotelId { get; set; }
+        public string hotelName { get; set; }
+        public string hotelPhone { get; set; }
+        public string hotelEmail { get; set; }
+        public string hotelAddress { get; set; }
+        public string hotelImage { get; set; }
+        public string hotelDescription { get; set; }
+        public bool isActive { get; set; } = true;
+        public bool isDelete { get; set; }
+        public string createdBy { get; set; }
+        public DateTime createdDate { get; set; }
+    }
+}
